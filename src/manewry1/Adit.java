1f244e0e89c7a7b470fc5480340b353a6d25c313
Jakub Krzysztof Niemyjski <01159059@pw.edu.pl>
2022-03-28 23:13:08 +0200

    Miejmy nadzieje, ze bedzie i folder manewry

---
+++ src/manewry1/Adit.java
@@ -0,0 +1,12 @@
+package manewry1;
+public class Adit {
+
+    private String name;
+    private int age;
+    
+    public static void main(String[] args) {
+    	System.out.println("no elo ziomo");
+    }
+
+
+}
